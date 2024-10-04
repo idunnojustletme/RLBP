d43e447dbe9ef0a91f41fa0b9a21b3906d06cba7
idunnojustletme <176437856+idunnojustletme@users.noreply.github.com>
2024-10-04 11:25:30 -0700

    Small config cleanup

--- load_config.py
+++ load_config.py
@@ -6,7 +6,5 @@ import yaml
 def load_config():
     with open("config.yaml", "r") as f:
         yaml_data = yaml.safe_load(f)
-        defaults = yaml_data.get("defaults", {})
         config = yaml_data.get("config", {})
-        merged_config = {**defaults, **config}
-        return merged_config
+        return config
